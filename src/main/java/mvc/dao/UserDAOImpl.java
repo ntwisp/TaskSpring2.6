fb3cdfc713f910c37d683b1c5b477f12dfea3867
ntwisp <liangush@yandex.ru>
2024-10-22 16:49:57 +0300

    repository

--- src/main/java/mvc/dao/UserDAOImpl.java
+++ src/main/java/mvc/dao/UserDAOImpl.java
@@ -1,13 +1,13 @@
 package mvc.dao;
 
 import mvc.model.User;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import java.util.List;
 
-@Component
+@Repository
 public class UserDAOImpl implements UserDAO {
 
     @PersistenceContext
